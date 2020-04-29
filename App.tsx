4a53e9a58c13d7658f08b3486bdc92ce8a9d19a4
João Amadeu <jmamadeu2000@gmail.com>
2020-04-29 10:54:34 +0100

    Configuração e instalação das dependências

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+import { AuthProvider } from './src/contexts/auth';
+
+import Routes from './src/routes/index.routes';
+
+import { View, Text } from 'react-native';
+
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
+    <NavigationContainer>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      {/* <View>
+        <Text>Meu APP</Text>
+      </View> */}
+    </NavigationContainer>
+  );
+};
+
+export default App;
